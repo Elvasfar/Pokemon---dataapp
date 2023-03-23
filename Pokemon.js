0a6db1708e64e5911d5635bdf4c28f9578e74f4e
Elvasfar <brondby_1964@hotmail.com>
2023-03-23 11:30:57 +0100

    billede

--- Pokemon.js
+++ Pokemon.js
@@ -3,10 +3,35 @@
 window.addEventListener("load", start);
 
 async function start() {
-  const SlowBro = await getPokemon("data/Slowbro.json");
+  console.log("start kører");
+  const SlowBro = await getPokemon(
+    "https://raw.githubusercontent.com/Elvasfar/Pokemon---dataapp/main/Slowbro.json"
+  );
   console.log(SlowBro);
+
+  showPokemon(SlowBro);
 }
 
-async function getPokemon() {}
+async function getPokemon(url) {
+  const response = await fetch(url);
+  console.log(response);
 
-function showPokemon() {}
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+function showPokemon(pokemon) {
+  document.querySelector("body").insertAdjacentHTML(
+    "beforeend",
+    `
+    <article>
+    <img src="${pokemon.image}" alt""/>
+    <h1>${pokemon.name}</h1>
+    <h2>${pokemon.category}</h2>
+    <p>Type: ${pokemon.type}</p>
+    <p>Weaknesses: ${pokemon.weaknesses}</p>
+    </article>
+    `
+  );
+}
